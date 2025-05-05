31464b4ec0f970ff4ebd6ee6b9d247d78f8de3a6
asus <swatikadam68@gmail.com>
2025-05-05 22:57:19 +0530

    Add new file

---
+++ src/Ex_01_Java_Basics/PrintNumbers_1_10.java
@@ -0,0 +1,11 @@
+package Ex_01_Java_Basics;
+
+public class PrintNumbers_1_10 {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
