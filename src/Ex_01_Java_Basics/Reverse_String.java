ed33e5c24a53c1beab6aead3bd204ac1eadd4f45
asus <swatikadam68@gmail.com>
2025-05-08 22:33:31 +0530

    Add new file

---
+++ src/Ex_01_Java_Basics/Reverse_String.java
@@ -0,0 +1,15 @@
+package Ex_01_Java_Basics;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        String str="Swati";
+        String reverse="";
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            reverse += str.charAt(i);
+        }
+        System.out.println("Reversed="+reverse);
+
+
+    }
+}
