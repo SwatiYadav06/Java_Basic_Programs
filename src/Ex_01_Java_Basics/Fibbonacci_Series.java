a99de9c829d7be38410e21a281c30e727cbcad43
asus <swatikadam68@gmail.com>
2025-05-08 22:07:18 +0530

    Add new file

---
+++ src/Ex_01_Java_Basics/Fibbonacci_Series.java
@@ -0,0 +1,18 @@
+package Ex_01_Java_Basics;
+
+public class Fibbonacci_Series {
+    public static void main(String[] args) {
+        {
+            int next,a=0,b=1;
+           System.out.println(a);
+            for(int i=0;i<=10;i++)
+            {
+                next=a+b;
+                System.out.println(next + " ");
+                a=b;
+                b=next;
+            }
+        }
+
+    }
+}
