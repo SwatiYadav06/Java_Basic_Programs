a3dbd69aa1679090b6938ea9eba7b0184cc5b9af
asus <swatikadam68@gmail.com>
2025-05-13 00:06:30 +0530

    Add new file

---
+++ src/Ex_02_Java_Basic_Part2/Lab028_Final_Example.java
@@ -0,0 +1,13 @@
+package Ex_02_Java_Basic_Part2;
+
+public class Lab028_Final_Example {
+    public static void main(String[] args) {
+        int local;
+        local = 10;
+        System.out.println(local);
+
+        final float PI = 3.14f;
+        // PI = 3.13f; This is final in nature.
+        System.out.println(PI);
+    }
+}
