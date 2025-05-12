942fc20798b93c3aeee158bf3a3c6264889854c2
asus <swatikadam68@gmail.com>
2025-05-13 00:06:09 +0530

    Add new file

---
+++ src/Ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,17 @@
+package Ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi, Swati");
+
+        // This is Swati, This code will not executed
+
+        /*
+         * Author :  Swati Yadav
+         *
+         *
+         * */
+
+
+    }
+}
