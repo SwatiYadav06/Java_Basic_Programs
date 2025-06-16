f90e9b08f79df0e39a175fd7f1ba124551e2d223
asus <swatikadam68@gmail.com>
2025-06-17 00:08:26 +0530

    Add new file

--- src/Tasks/FirstLetterString.java
+++ src/Tasks/FirstLetterString.java
@@ -10,10 +10,14 @@ public class FirstLetterString {
         String lot = words.trim().replaceAll("\\s+", " ");
         String[] real = lot.split(" ");
         // System.out.println(real);
-        for(String name : real){
+       /* for(String name : real){
             if(!name.isEmpty()){
                 System.out.print(name.charAt(0));
             }
+        }*/
+        for (int i = 0; i < real.length; i++) {
+            String word = real[i];
+            System.out.println(word.charAt(i));
         }
 
     }
